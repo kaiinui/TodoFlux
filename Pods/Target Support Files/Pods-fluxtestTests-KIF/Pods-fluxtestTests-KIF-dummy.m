26e86ebc5b75bfe7051ff4f8d384c1b78d966ddd
Kai INUI <lied.der.optik@gmail.com>
2014-10-19 15:51:28 +0900

    rm Pods

--- Pods/Target Support Files/Pods-fluxtestTests-KIF/Pods-fluxtestTests-KIF-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_fluxtestTests_KIF : NSObject
-@end
-@implementation PodsDummy_Pods_fluxtestTests_KIF
-@end
--- Pods/Target Support Files/Pods-fluxtestTests-OCHamcrest/Pods-fluxtestTests-OCHamcrest-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_fluxtestTests_OCHamcrest : NSObject
-@end
-@implementation PodsDummy_Pods_fluxtestTests_OCHamcrest
-@end
